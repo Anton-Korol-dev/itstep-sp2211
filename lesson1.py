58bffe95cbf6345c501265737ea76fefe025c716
Anton-Korol-dev <antony.prime.95@gmail.com>
2025-09-24 19:12:52 +0300

    classes in python

--- lesson1.py
+++ lesson1.py
@@ -1,1 +1,20 @@
-print("Hello World")
+class Student:
+    # amount_of_students = 0
+    # height = 170
+    def __init__(self):
+        # self.height = height
+        # Student.amount_of_students += 1
+        # print(self.height)
+        self.height = 230
+    height = 200
+    def printer(self):
+        print(self.height)
+
+Ira = Student()
+Ira.printer()
+# print(Ira.height)
+# print(Oleksandra.height)
+
+# print(Ira.amount_of_students)
+# print(Oleksandra.amount_of_students)
+

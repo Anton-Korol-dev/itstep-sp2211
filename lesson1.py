9f4d52e74a37d84a3e65dcf11b3ebfa5a9ccc0da
Anton-Korol-dev <antony.prime.95@gmail.com>
2025-09-24 19:31:28 +0300

    models in python middle part

--- lesson1.py
+++ lesson1.py
@@ -1,14 +1,23 @@
 class Student:
-    amount_of_students = 0
-    def __init__(self, height = 170):
+    def __init__(self, name=None, height=170):
+        self.name = name
         self.height = height
-        Student.amount_of_students += 1
-    def grow(self, height):
-        self.height += height
 
-Ira = Student()
-print("Ira height before growing: ", Ira.height)
-Andriy = Student(height=180)
-Ira.grow(height=5)
-print("Ira height after growing: ", Ira.height)
-print(Andriy.height)
+#     def __del__(self):
+#         print(f"Student {self.name} is being deleted")
+
+# Nick = Student("Nick")
+# del Nick
+    def __bool__(self):
+        # return bool(self.name)
+        return self.name != None
+    
+    def __len__(self):
+        return self.height
+    
+Nick = Student("Nick")
+print(Nick.__len__())
+print(Nick.__bool__())
+print(len(Nick))
+print(bool(Nick))
+

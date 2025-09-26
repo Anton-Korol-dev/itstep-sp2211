4570c631ad8110169068680ad920eb0fa466ed65
Anton-Korol-dev <toha20031502@gmail.com>
2025-09-26 23:01:10 +0300

    v.3

--- lesson4.py
+++ lesson4.py
@@ -3,14 +3,30 @@
 # class Child(Parent):
 #     classmethods and attrs
 
-class Human:
-    height = 170
-class Student(Human):
-    satiety = 0
-class Worker(Human):
-    satiety = 100
+# class Human:
+#     height = 170
+#     satiety = 50
+# class Student(Human):
+#     satiety = 0
+# class Worker(Human):
+#     satiety = 100
 
-nick = Student()
-ann = Worker()
-print(nick.satiety)
-print(ann.satiety)
+# nick = Student()
+# ann = Worker()
+# print(nick.satiety)
+# print(ann.satiety)
+
+class Grandparent:
+    height = 170
+    satiety = 100
+    age = 60
+class Parent(Grandparent):
+    age = 40
+
+class Child(Parent):
+    height = 50
+    def __init__(self):
+        print(self.height)
+        print(self.satiety)
+        print(self.age)
+nick = Child()

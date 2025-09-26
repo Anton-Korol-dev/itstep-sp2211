26eefa95165f349c57c50fab8393060d8b604804
Anton-Korol-dev <toha20031502@gmail.com>
2025-09-26 23:21:28 +0300

    v.5

--- lesson4.py
+++ lesson4.py
@@ -31,32 +31,68 @@
 #         print(self.age)
 # nick = Child()
 
-class Hello_world:
-    hello = "Hello"
-    _hello = "_Hello"
-    __hello = "__Hello"
+# class Hello_world:
+#     hello = "Hello"
+#     _hello = "_Hello"
+#     __hello = "__Hello"
+#     def __init__(self):
+#         self.world = "World"
+#         self._world = "_World"
+#         self.__world = "__World"
+#     def printer(self):
+#         print(self.hello)
+#         print(self._hello)
+#         print(self.__hello)
+#         print(self.world)
+#         print(self._world)
+#         print(self.__world)
+
+# class Hi(Hello_world):
+#     def hi_print(self):
+#         print(self.hello)
+#         print(self.world)
+#         print(self._hello)
+#         print(self._world)
+#         print(self.__hello)
+#         print(self.__world)
+
+# hello = Hello_world()
+# hello.printer()
+# hi = Hi()
+# hi.hi_print()
+
+# class Hello:
+#     def __init__(self):
+#         print("Hello!")
+# class Hello_World(Hello):
+#     def __init__(self):
+#         super().__init__()
+#         print("World!")
+# hello_world = Hello_World()
+
+# class Class1:
+#     var = 20
+#     def __init__(self):
+#         self.var = 10
+# class Class2(Class1):
+#     def __init__(self):
+#         print(self.var)
+#         super().__init__()
+#         print(self.var)
+#         print(super().var)
+# obj = Class2()
+
+class Grandparent:
+    def about(self):
+        print("I am GrandParent")
+    def about_myself(self):
+        print("I am Grandparent")
+class Parent(Grandparent):
+    def about_myself(self):
+        print("I am Parent")
+class Child(Parent):
     def __init__(self):
-        self.world = "World"
-        self._world = "_World"
-        self.__world = "__World"
-    def printer(self):
-        print(self.hello)
-        print(self._hello)
-        print(self.__hello)
-        print(self.world)
-        print(self._world)
-        print(self.__world)
+        super().about()
+        super().about_myself()
 
-class Hi(Hello_world):
-    def hi_print(self):
-        print(self.hello)
-        print(self.world)
-        print(self._hello)
-        print(self._world)
-        print(self.__hello)
-        print(self.__world)
-
-hello = Hello_world()
-hello.printer()
-hi = Hi()
-hi.hi_print()
+nick = Child()
